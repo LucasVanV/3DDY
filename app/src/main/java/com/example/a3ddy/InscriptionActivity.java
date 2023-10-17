94ea52a7d5412ebd1d48704ca1f674aa6309afc4
Theo Vesque <theovesque171201@gmail.com>
2023-10-17 16:10:06 +0200

    XML page inscription

--- app/src/main/java/com/example/a3ddy/InscriptionActivity.java
+++ app/src/main/java/com/example/a3ddy/InscriptionActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class InscriptionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_inscription);
+
+        ImageView imageView = findViewById(R.id.imageView);
     }
 }
