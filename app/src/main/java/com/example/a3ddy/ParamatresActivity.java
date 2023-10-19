5ea7a1edc638073f68a4bab952b7c6b30792a3f2
Theo Vesque <theovesque171201@gmail.com>
2023-10-19 12:10:45 +0200

    Création de la page de modification du compte

--- app/src/main/java/com/example/a3ddy/ParamatresActivity.java
+++ app/src/main/java/com/example/a3ddy/ParamatresActivity.java
@@ -2,6 +2,9 @@ package com.example.a3ddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class ParamatresActivity extends AppCompatActivity {
@@ -10,5 +13,17 @@ public class ParamatresActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paramatres);
+
+        Button boutonMonCompte = findViewById(R.id.button);
+
+        boutonMonCompte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_mon_compte = new Intent(ParamatresActivity.this, MonCompteActivity.class);
+                startActivity(intent_mon_compte);
+            }
+        });
     }
+
+
 }
