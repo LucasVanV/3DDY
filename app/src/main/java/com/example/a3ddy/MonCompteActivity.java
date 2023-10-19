0cdfa39b4930064a53436b07f0bf2eebce92c555
Theo Vesque <theovesque171201@gmail.com>
2023-10-19 13:57:12 +0200

    Ajout du bouton retour sur la pagr mon compte et modification button en edittext

--- app/src/main/java/com/example/a3ddy/MonCompteActivity.java
+++ app/src/main/java/com/example/a3ddy/MonCompteActivity.java
@@ -2,7 +2,10 @@ package com.example.a3ddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MonCompteActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MonCompteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mon_compte);
+
+        ImageButton boutonRetour = findViewById(R.id.retour);
+
+        boutonRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_mon_compte = new Intent(MonCompteActivity.this, ParamatresActivity.class);
+                startActivity(intent_mon_compte);
+            }
+        });
     }
 }
