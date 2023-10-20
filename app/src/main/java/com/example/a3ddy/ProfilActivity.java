54719c987b23691d4e043e7ef43ccea94e3104bd
Theo Vesque <theovesque171201@gmail.com>
2023-10-20 14:47:13 +0200

    Début de création du menu

--- app/src/main/java/com/example/a3ddy/ProfilActivity.java
+++ app/src/main/java/com/example/a3ddy/ProfilActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class ProfilActivity extends AppCompatActivity {
+    private static final int ACCUEIL = 1;
+    private static final int RECHERCHE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +28,25 @@ public class ProfilActivity extends AppCompatActivity {
                 startActivity(intent_parametre);
             }
         });
+
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        bottomNavigation.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case ACCUEIL:
+                        // Réagir au clic sur l'élément 1
+                        return true;
+                    case RECHERCHE:
+                        // Réagir au clic sur l'élément 2
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
     }
 }
