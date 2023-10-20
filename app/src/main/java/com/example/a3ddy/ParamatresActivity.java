ed77021d24e05995ad30aac71f4579fb89fcb4b7
Theo Vesque <theovesque171201@gmail.com>
2023-10-20 11:45:54 +0200

    Correction page de profil et création des intent pour les boutons

--- app/src/main/java/com/example/a3ddy/ParamatresActivity.java
+++ app/src/main/java/com/example/a3ddy/ParamatresActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class ParamatresActivity extends AppCompatActivity {
 
@@ -15,12 +16,21 @@ public class ParamatresActivity extends AppCompatActivity {
         setContentView(R.layout.activity_paramatres);
 
         Button boutonMonCompte = findViewById(R.id.button);
+        ImageButton boutonRetour = findViewById(R.id.retour);
 
         boutonMonCompte.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent_mon_compte = new Intent(ParamatresActivity.this, MonCompteActivity.class);
                 startActivity(intent_mon_compte);
+            }
+        });
+
+        boutonRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_profil = new Intent(ParamatresActivity.this, ProfilActivity.class);
+                startActivity(intent_profil);
             }
         });
     }
--- app/src/main/java/com/example/a3ddy/ProfilActivity.java
+++ app/src/main/java/com/example/a3ddy/ProfilActivity.java
@@ -2,7 +2,10 @@ package com.example.a3ddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ProfilActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class ProfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        ImageButton bouton_parametre = findViewById(R.id.bouton_parametre);
+
+        bouton_parametre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_parametre = new Intent(ProfilActivity.this, ParamatresActivity.class);
+                startActivity(intent_parametre);
+            }
+        });
     }
 }
