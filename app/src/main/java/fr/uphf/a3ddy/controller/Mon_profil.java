a43e77e55c03606e62e116c22e1c2fb4ed34a4d8
Theo Vesque <theovesque171201@gmail.com>
2023-11-16 10:42:14 +0100

    Modification xml modification de profil

---
+++ app/src/main/java/fr/uphf/a3ddy/controller/Mon_profil.java
@@ -0,0 +1,16 @@
+package fr.uphf.a3ddy.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.uphf.a3ddy.R;
+
+public class Mon_profil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mon_profil);
+    }
+}
